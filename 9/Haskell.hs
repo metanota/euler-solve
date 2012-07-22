5ab6cc69302cbb4815cf06d3396526464b5c58da
Shaggie <metanota@gmail.com>
2012-07-23 00:34:58 +0400

    Problem #9 Haskell solved (II). Much faster as third value is already known at the creation time

--- 9/Haskell.hs
+++ 9/Haskell.hs
@@ -1,8 +1,5 @@
 
 isTriplet a b c = a < b && b < c && a*a + b*b == c*c
 
-isSolved a b c = a + b + c == 1000
-
 main = do
-       print $ product $ head $ [[x,y,z] | x<-[1..1000], y<-[1..1000], z<-[1..1000],
-                                           isTriplet x y z && isSolved x y z]
+       print $ product $ head $ [[x,y,z] | x<-[1..1000], y<-[1..1000], z<-[1000 - x - y], isTriplet x y z]
