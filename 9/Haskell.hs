ad4c5fdd4e0d87738a3e046e8c0a6cea571dd5ad
Shaggie <metanota@gmail.com>
2012-07-23 00:37:22 +0400

    Problem #9 Haskell solved (III). A little optimization

--- 9/Haskell.hs
+++ 9/Haskell.hs
@@ -2,4 +2,4 @@
 isTriplet a b c = a < b && b < c && a*a + b*b == c*c
 
 main = do
-       print $ product $ head $ [[x,y,z] | x<-[1..1000], y<-[1..1000], z<-[1000 - x - y], isTriplet x y z]
+       print $ product $ head $ [[x,y,z] | x<-[1..1000], y<-[x+1..1000], z<-[1000 - x - y], isTriplet x y z]
