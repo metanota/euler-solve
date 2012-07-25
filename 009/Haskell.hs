c8e3c39928a20281b9c7c0475ff0da66c5ed6ba9
Shaggie <metanota@gmail.com>
2012-07-26 00:25:11 +0400

    Problem #9 Haskell solved (IV). Removed useless check

--- 009/Haskell.hs
+++ 009/Haskell.hs
@@ -1,5 +1,8 @@
 
-isTriplet a b c = a < b && b < c && a*a + b*b == c*c
+isTriplet a b c = a*a + b*b == c*c
 
 main = do
-       print $ product $ head $ [[x,y,z] | x<-[1..1000], y<-[x+1..1000], z<-[1000 - x - y], isTriplet x y z]
+       print $ product $ head $ [[x,y,z] | x<-[1..1000],
+                                           y<-[x+1..1000],
+                                           z<-[1000 - x - y],
+                                           isTriplet x y z]
