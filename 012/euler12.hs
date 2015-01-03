00722166d8ca1aacd12210c681380d9a5e45b956
Shaggie <metanota@gmail.com>
2015-01-03 18:09:29 +0300

    Problem #12 Haskell solved

---
+++ 012/euler12.hs
@@ -0,0 +1,13 @@
+import Control.Applicative ((<*>))
+import Data.List           (group)
+import Data.Numbers.Primes (primeFactors)
+
+main = do
+       print $ head $ filter ((> 500) . snd) $ zipF divisorsCount triangles
+
+-- | http://en.wikipedia.org/wiki/Divisor_function
+divisorsCount = product . map (+1) . map (length) . group . primeFactors
+
+triangles = scanl (+) 1 [2..]
+
+zipF f = zip <*> map f
